37c652681b81fd6a109b86a9fc800a1bf6e44991
Santiago Moritan <smoritan@icloud.com>
2019-10-06 20:29:13 -0300

    localStorage, refactoring, ul-li, changed desing, changed var for let and const, changed colors and add a diferent id for each item

--- script.js
+++ script.js
@@ -1,44 +1,132 @@
+const createButton = document.getElementById('create');
+const listElement = document.getElementById('list');
 
-items = [];
+let items = [];
 
-function obtenerUltimoId() {
-    var maxId = 0;
-    for(var i=0;i<items.length;++i){
-      if(items[i].id < maxId)
-        maxId = items[i].id;
+
+function renderItem(item) {
+    const textContainer = document.createElement('span');
+    textContainer.textContent = item.content;
+
+    const editButton = document.createElement('button');
+    editButton.textContent = 'E';
+    editButton.addEventListener('click', editItem);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'D';
+    deleteButton.addEventListener('click', deleteItem);
+
+    const itemElement = document.createElement('li');
+    itemElement.dataset.id = item.id;
+    itemElement.appendChild(textContainer);
+    itemElement.appendChild(editButton);
+    itemElement.appendChild(deleteButton);
+
+    listElement.appendChild(itemElement);
+}
+
+
+function createItem() {
+    const content = window.prompt('Ingrese el contenido');
+
+    const item = {
+        id: items.length + 1,
+        content: content
     }
-    return maxId++;
-  };
 
-function editar(){
-    document.getElementById('items').contentEditable ='true';
-    document.getElementById('items').designMode='on';
-    guardar();
-};
+    items.push(item);
+    renderItem(item);
 
-function crear() {
-    var id = obtenerUltimoId();
-    var c = document.getElementById("nuevoItem");
-    var clon = c.cloneNode(true);
-    items.push(clon);
-    clon.setAttribute("id", id);
-    console.log(items)
-    document.getElementById("list").appendChild(clon);
-    guardar();
-  };
+    saveItems();
+}
 
-function eliminar(id){
-    var node = document.getElementById(obtenerUltimoId(id));
-    node.parentNode.removeChild(node);
-    guardar();
-};
+createButton.addEventListener('click', createItem);
 
-/* function status(){
-  var estado = document.white("Longitud del array: " + items.length);
-  document.getElementById("list").appendChild(estado);
-};
-status(); */
 
-function guardar(){
-  localStorage.setItem('myItems', JSON.stringify(items));
-};
+function editItem(event) {
+    const itemElement = event.target.parentElement;
+    const index = items.findIndex(item => item.id === parseInt(itemElement.dataset.id));
+    const content = window.prompt('Edite el contenido', items[index].content);
+
+    items[index].content = content;
+    itemElement.firstChild.textContent = content;
+
+    saveItems();
+}
+
+
+function deleteItem(event) {
+    if (confirm('¿Eliminar el contenido?')) {
+        const itemElement = event.target.parentElement;
+        const index = items.findIndex(item => item.id === parseInt(itemElement.dataset.id));
+
+        delete items[index];
+
+        itemElement.parentElement.removeChild(itemElement);
+
+        saveItems();
+    }
+}
+
+
+function saveItems() {
+    window.localStorage.setItem('items', JSON.stringify(items));
+}
+
+function recoverItems() {
+    const recoveredItems = window.localStorage.getItem('items');
+
+    if (recoveredItems) {
+        items = JSON.parse(recoveredItems);
+
+        for (let item of items) {
+            if (item) {
+                renderItem(item);
+            }
+        }
+    }
+}
+
+recoverItems();
+
+
+// function obtenerUltimoId() {
+//     var maxId = -1;
+//     for(var i=0;i<this.items.length;++i){
+//       if(this.items[i].id < maxId)
+//         maxId = this.items[i].id;
+//     }
+//     return maxId++;
+//   };
+
+// function editar(){
+//     document.getElementById('items').contentEditable ='true';
+//     document.getElementById('items').designMode='on';
+// };
+
+// function crear() {
+//     var id = obtenerUltimoId();
+//     var c = document.getElementById("nuevoItem");
+//     var clon = c.cloneNode(true);
+//     items.push(clon);
+//     clon.setAttribute("id", obtenerUltimoId(id));
+//     console.log(items)
+//     document.getElementById("list").appendChild(clon);
+//     guardar();
+//   };
+
+// function eliminar(id){
+//     var node = document.getElementById(obtenerUltimoId(id));
+//     node.parentNode.removeChild(node);
+//     localStorage.removeItem('myItems');
+// };
+
+
+// if (typeof(Storage) !== 'undefined') {
+//   function guardar(){
+//     localStorage.setItem('myItems', JSON.stringify(items));
+//     var guardado = localStorage.getItem('myItems');
+//     console.log(guardado);
+//   };
+// } else {
+// }
